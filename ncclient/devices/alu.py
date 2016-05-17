33e2fd405ee25d2f76b1962748f494c3a75854a4
Jaime Botello <jbotello@riotgames.com>
2016-05-17 11:09:27 -0700

    Include RPC method to alu.py device handler

--- ncclient/devices/alu.py
+++ ncclient/devices/alu.py
@@ -1,14 +1,21 @@
 from .default import DefaultDeviceHandler
+from ncclient.operations.third_party.juniper.rpc import ExecuteRpc
 
 class AluDeviceHandler(DefaultDeviceHandler):
     """
     Alcatel-Lucent 7x50 handler for device specific information.
     """
-    
+
     def __init__(self, device_params):
         super(AluDeviceHandler, self).__init__(device_params)
+
+    def add_additional_operations(self):
+        dict = {}
+        dict["rpc"] = ExecuteRpc
+        return dict
 
     def get_capabilities(self):
         return [
             "urn:ietf:params:netconf:base:1.0",
         ]
+
